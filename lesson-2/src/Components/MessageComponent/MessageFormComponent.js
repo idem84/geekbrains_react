6568e77980470ec35b15c29181285bbaeb1ac120
Dmitriy <idem84@me.com>
2021-09-14 22:38:15 +0300

    hw2 done

---
+++ lesson-2/src/Components/MessageComponent/MessageFormComponent.js
@@ -0,0 +1,53 @@
+import React from "react";
+import './module.style.scss';
+
+const MessageFormComponent = ({ sendMessage }) => {
+    const [state, setState] = React.useState({
+        author: "",
+        message: ""
+    })
+
+    const handleChange = (evt) => {
+        const value = evt.target.value;
+
+        setState({
+            ...state,
+            [evt.target.name]: value
+        });
+    }
+
+    const handleSubmit = (evt) => {
+        evt.preventDefault()
+        sendMessage(state)
+        setState({
+            author: "",
+            message: ""
+        });
+      }
+
+    return (
+        <form onSubmit={handleSubmit}>
+            <label>
+                Author
+                <input
+                    type="text"
+                    name="author"
+                    value={state.author}
+                    onChange={handleChange}
+                />
+            </label>
+            <label>
+                Message
+                <input
+                    type="text"
+                    name="message"
+                    value={state.message}
+                    onChange={handleChange}
+                />
+            </label>
+            <button>Add</button>
+        </form>
+    );
+}
+
+export default MessageFormComponent;
