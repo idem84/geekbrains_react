e218eb6a989329c276e02ef8149ab1ee357c4a73
Dmitriy <idem84@me.com>
2021-09-27 17:13:56 +0300

    clone lesson-4 to lesson-5

---
+++ lesson-5/src/Components/MainComponent/MainComponent.js
@@ -0,0 +1,16 @@
+import Box from "@material-ui/core/Box";
+
+const MainComponent = () => {
+  return (
+    <Box component="main" sx={{padding: '20px'}}>
+        <h1>Main page</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore distinctio ipsum nam porro! Eaque impedit laudantium dolore fuga enim ea repellat, maiores magni atque ut, culpa asperiores. Quasi, debitis vitae.
+            Et quod id iusto repellat molestias repudiandae pariatur, quas veritatis officiis placeat vel. Est ipsum expedita odio perferendis repellat officia sint quod, non velit iusto asperiores id dignissimos earum nobis.
+            Esse molestias aspernatur culpa earum voluptatibus voluptatum perspiciatis fuga expedita natus, perferendis error aliquam explicabo aut voluptatem, rem, laborum nam. Doloremque velit asperiores enim beatae illo odio distinctio totam iusto.
+            Ipsum facere ipsa amet quos, voluptatum dignissimos veniam atque a exercitationem, voluptas magnam consequuntur natus distinctio aliquid id libero ut. Blanditiis culpa fuga voluptate beatae eius voluptates iste vitae itaque.
+            Tempora excepturi sapiente eum perspiciatis aperiam aliquam, qui nobis doloribus placeat error aliquid. Voluptate beatae in ea, ullam totam repudiandae ipsa dicta quasi animi quas dolores nostrum error laudantium impedit.</p>
+    </Box>
+  );
+};
+
+export default MainComponent;
